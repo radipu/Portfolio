6602966a7ab6a2303b6531a155022e4d047d8546
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-18 13:21:07 +0600

    update js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -414,64 +414,29 @@
 				coll[j].classList.remove("active");
 			}
     		this.classList.toggle("active");
-    		
-    		// if (content.style.maxHeight){
-      		// 	content.style.maxHeight = null;
-    		// }
-			// else {
-      		// 	content.style.maxHeight = content.scrollHeight + "px";
-    		// }
   			});
 		}
-		
-		//image slideshow at graphics designer
-		// var slideIndex = 1;
-		// showSlides(slideIndex);
-
-		// function plusSlides(n) {
-  		// 	showSlides(slideIndex += n);
-		// }
-
-		// function currentSlide(n) {
-  		// 	showSlides(slideIndex = n);
-		// }
-
-		// function showSlides(n) {
-  		// 	var i;
-  		// 	var slides = document.getElementsByClassName("mySlides");
-  		// 	var dots = document.getElementsByClassName("dot");
-  		// 	if (n > slides.length) {slideIndex = 1}    
-  		// 	if (n < 1) {slideIndex = slides.length}
-  		// 	for (i = 0; i < slides.length; i++) {
-      	// 		slides[i].style.display = "none";  
-  		// 	}
-  		// 	for (i = 0; i < dots.length; i++) {
-      	// 		dots[i].className = dots[i].className.replace(" active", "");
-  		// 	}
-  		// 	slides[slideIndex-1].style.display = "block";  
-  		// 	dots[slideIndex-1].className += " active";
-		// }
 
 //automated slideshow every 2 seconds
-var slideIndex = 0;
-showSlides();
+		var slideIndex = 0;
+		showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+		function showSlides() {
+  		var i;
+  		var slides = document.getElementsByClassName("mySlides");
+  		var dots = document.getElementsByClassName("dot");
+  		for (i = 0; i < slides.length; i++) {
+    		slides[i].style.display = "none";  
+  		}
+  		slideIndex++;
+  		if (slideIndex > slides.length) {slideIndex = 1}    
+  		for (i = 0; i < dots.length; i++) {
+    		dots[i].className = dots[i].className.replace(" active", "");
+  		}
+  		slides[slideIndex-1].style.display = "block";  
+  		dots[slideIndex-1].className += " active";
+  		setTimeout(showSlides, 5000); // Change image every 2 seconds
+	}
 		
 
 })(jQuery);
